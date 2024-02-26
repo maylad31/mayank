55e8574176c6d297af090b7ce2498a5b8a656dfc
maylad31 <mynameladdha@gmail.com>
2024-02-26 20:28:57 +0530

    cleaned

--- js/script.js
+++ js/script.js
@@ -1,204 +1,164 @@
-// on ready state
-$(document).ready(function () {
-  "use strict";
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 0) {
-      $(".navigation").addClass("fixed-menu ");
-    } else {
-      $(".navigation").removeClass("fixed-menu ");
-    }
-    if ($(window).scrollTop() > 1000) {
-      $(".backtotop").fadeIn();
-    } else {
-      $(".backtotop").fadeOut();
-    }
-  });
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
 
-  $(".backtotop").click(function () {
-    $("html,body").animate({
-      scrollTop: 0,
+(function ($) {
+    'use strict';
+
+
+
+    // Sticky Menu
+    $(window).scroll(function () {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
     });
-  });
 
-  // brand log slider
-  $(".brand-logo-slider").slick({
-    dots: false,
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    fade: false,
-    speed: 300,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 991,
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
 
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          dots: false,
-          infinite: true,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          dots: false,
-          infinite: true,
-          arrows: false,
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          dots: false,
-          infinite: true,
-          arrows: false,
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
+    // background color
+    $('[data-color]').each(function () {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
 
-  // Testimonial-Slider
-  $(".testimonial-left-slider").slick({
-    dots: false,
-    infinite: true,
-    arrows: true,
-    autoplay: false,
-    prevArrow: '<i class="fas fa-arrow-left prev-arrow"></i>',
-    nextArrow: '<i class="fas fa-arrow-right next-arrow"></i>',
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: ".testimonial-right-slider",
-    responsive: [
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: false,
-          infinite: true,
-          autoplay: false,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          dots: false,
-          infinite: true,
-          arrows: false,
-          autoplay: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          dots: false,
-          infinite: true,
-          arrows: false,
-          autoplay: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-  $(".testimonial-right-slider").slick({
-    dots: false,
-    infinite: true,
-    arrows: false,
-    autoplay: false,
-    fade: true,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: ".testimonial-left-slider",
-    responsive: [
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          autoplay: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          autoplay: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-});
+    // progress bar
+    $('[data-progress]').each(function () {
+        $(this).css({
+            'bottom': $(this).data('progress')
+        });
+    });
 
-// Preloader
-//   setTimeout(function () {
-//     $(".preloader").fadeOut()
-//   }, 1500);
 
-// });
+    /* ########################################### hero parallax ############################################## */
+    window.onload = function () {
 
-// AOS.init();
+        var parallaxBox = document.getElementById('parallax');
+        var
+            /* c1left = document.getElementById('l1').offsetLeft,
+                       c1top = document.getElementById('l1').offsetTop, */
+            c2left = document.getElementById('l2').offsetLeft,
+            c2top = document.getElementById('l2').offsetTop,
+            c3left = document.getElementById('l3').offsetLeft,
+            c3top = document.getElementById('l3').offsetTop,
+            c4left = document.getElementById('l4').offsetLeft,
+            c4top = document.getElementById('l4').offsetTop,
+            c5left = document.getElementById('l5').offsetLeft,
+            c5top = document.getElementById('l5').offsetTop,
+            c6left = document.getElementById('l6').offsetLeft,
+            c6top = document.getElementById('l6').offsetTop,
+            c7left = document.getElementById('l7').offsetLeft,
+            c7top = document.getElementById('l7').offsetTop,
+            c8left = document.getElementById('l8').offsetLeft,
+            c8top = document.getElementById('l8').offsetTop,
+            c9left = document.getElementById('l9').offsetLeft,
+            c9top = document.getElementById('l9').offsetTop;
 
-$(window).on("load", function () {
-  "use strict";
-  // preloader init
-  setTimeout(function () {
-    $(".preloader").fadeOut(200);
+        parallaxBox.onmousemove = function (event) {
+            event = event || window.event;
+            var x = event.clientX - parallaxBox.offsetLeft,
+                y = event.clientY - parallaxBox.offsetTop;
 
-    // aos scroll-animation Init
-    function aosAnim() {
-      AOS.init({
-        duration: 600,
-        once: true,
-      });
+            /*  mouseParallax('l1', c1left, c1top, x, y, 5); */
+            mouseParallax('l2', c2left, c2top, x, y, 25);
+            mouseParallax('l3', c3left, c3top, x, y, 20);
+            mouseParallax('l4', c4left, c4top, x, y, 35);
+            mouseParallax('l5', c5left, c5top, x, y, 30);
+            mouseParallax('l6', c6left, c6top, x, y, 45);
+            mouseParallax('l7', c7left, c7top, x, y, 30);
+            mouseParallax('l8', c8left, c8top, x, y, 25);
+            mouseParallax('l9', c9left, c9top, x, y, 40);
+        };
+
+    };
+
+    function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+        var obj = document.getElementById(id);
+        var parentObj = obj.parentNode,
+            containerWidth = parseInt(parentObj.offsetWidth),
+            containerHeight = parseInt(parentObj.offsetHeight);
+        obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px';
+        obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
     }
-    setTimeout(function () {
-      aosAnim();
-    }, 100);
-  }, 1500);
-});
+    /* ########################################### /hero parallax ############################################## */
 
-$(document).ready(function () {
-  "use strict";
+    // testimonial-slider
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        adaptiveHeight: true
+    });
 
-  var Shuffle = window.Shuffle;
 
-  var myShuffle = new Shuffle(document.querySelector(".my-shuffle"), {
-    itemSelector: ".image-item",
-    sizer: ".my-sizer-element",
-    buffer: 1,
-  });
+    // clients logo slider
+    $('.client-logo-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 
-  window.jQuery('input[name="shuffle-filter"]').on("change", function (evt) {
-    var input = evt.currentTarget;
-    if (input.checked) {
-      myShuffle.filter(input.value);
-    }
-  });
+    // Shuffle js filter and masonry
+    var Shuffle = window.Shuffle;
+    var jQuery = window.jQuery;
 
-  $(".portfolio-filters .btn").click(function () {
-    $(this).addClass("active").siblings().removeClass("active");
-  });
-});
+    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+        itemSelector: '.shuffle-item',
+        buffer: 1
+    });
+
+    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+        var input = evt.currentTarget;
+        if (input.checked) {
+            myShuffle.filter(input.value);
+        }
+    });
+
+
+
+})(jQuery);
